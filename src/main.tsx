739e95fa40e85956de9d92cb68b16442db630320
Raphael <Rafache@users.noreply.github.com>
2026-08-09 15:15:36 +0200

    Remove CI annotation test error

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,6 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
 
-const ciAnnotationTest: string = 123;
-void ciAnnotationTest;
-
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
